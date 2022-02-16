df7fcd53902afc432440b3d0ba83ede87329d8bd
Z3RO <99695665+z3ro0fc@users.noreply.github.com>
2022-02-15 22:04:58 -0300

    Create sms.py

---
+++ sms.py
@@ -0,0 +1,40 @@
+from colorama import Fore, init
+init()
+
+#$$("colors")$$
+
+rojo = Fore.RED
+violeta = Fore.MAGENTA
+azul = Fore.BLUE
+cyan = Fore.CYAN
+blanco = Fore.WHITE
+
+#$$("port")$$
+
+print(f"""
+{blanco}
+░██████╗███╗░░░███╗░██████╗
+██╔════╝████╗░████║██╔════╝{blanco}
+{rojo}╚█████╗░██╔████╔██║╚█████╗░
+░╚═══██╗██║╚██╔╝██║░╚═══██╗{rojo} {blanco}
+██████╔╝██║░╚═╝░██║██████╔╝
+╚═════╝░╚═╝░░░░░╚═╝╚═════╝░{blanco}
+
+{rojo}|{rojo}{cyan} Spam Sms creado por Z3RO{cyan}
+{violeta}|{violeta}{cyan} Spam Sms Traba telefono{cyan}
+{cyan}|{cyan} {rojo}Spam poderoso{rojo}
+
+{cyan}~~~{cyan}{rojo}>{rojo}{violeta} Spam enantenimiento solo dara poco spam por 3 minutos
+
+
+{violeta}""")
+
+#spam sms
+
+spam = input("Coloca el numero ==")
+if not True:
+           print(" El numero eata mal")
+else:
+    for i in range(10000000000000):
+        print(f"""{violeta}[{violeta}{rojo}+{rojo}{violeta}]{violeta}{cyan} Estamos Accediendo al dispositivo{cyan}
+{violeta}[{violeta}{rojo}+{rojo}{violeta}]{violeta}{cyan} El dispositivo Fue Hackeado {cyan}""")
